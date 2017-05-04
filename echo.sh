aad86bd333076902de0209d03a757440d46f13b7
shiphitchcock4 <shiphitchcock4@gmail.com>
2017-05-04 14:08:56 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,7 +1,4 @@
 echo ' ---> Hello rr1testing scriptedit'
 echo ' ---> testingtest1!!n1'
 #sleep 2m
-echo $SAMPLE-PUBLIC2_IS_FORK
-echo $SAMPLE-PUBLIC2_IS_PULL_REQUEST
-echo $SAMPLE-PUBLIC2_REPO_FULL_NAME
-echo $SAMPLE-PUBLIC2_PULL_REQUEST_REPO_FULL_NAME
+printenv
