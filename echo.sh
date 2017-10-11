326473e3924cb46556f1f9ddefe625105d9ff580
shiphitchcock4 <shiphitchcock4@gmail.com>
2017-10-11 19:49:21 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,4 +1,4 @@
 echo ' ---> Hello rr1testing scriptedit!!!!!!test'
-echo ' ---> testingtest1!3test123test23test12tes2t45'
-sleep 5m
+echo ' ---> testingtest1!3test123test23test12tes2t45123'
+#sleep 5m
 
