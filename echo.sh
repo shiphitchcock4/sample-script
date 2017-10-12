0b049e9d282e7d9236d448249a72efc5198146c7
shiphitchcock4 <shiphitchcock4@gmail.com>
2017-10-12 11:59:18 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,6 +1,8 @@
 echo ' ---> Hello rr1testing scriptedit!!!!!!test'
 echo ' ---> testingtest1!3test123test23test12tes2t4512312'
 #sleep 5m
+
+
 echo "-----> job triggeredBy"
 echo JobTriggredByName=$JOB_TRIGGERED_BY_NAME
 echo JobTriggredById=$JOB_TRIGGERED_BY_ID
